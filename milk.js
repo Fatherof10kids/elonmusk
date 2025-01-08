b18ad9442c648bc0b572924c25b4387044dd44c9
dung.tran <dung.tran@lge.com>
2025-01-08 16:46:16 +0700

    wrecking ball and cars added

--- milk.js
+++ milk.js
@@ -1,5 +1,4 @@
 
-const MAX_CIRCLES = 200;  // 1 litre = 6.5 circles
 
 window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.querySelector('#matter-canvas')
@@ -7,11 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
   let canvasWidth = canvas.offsetWidth
   let canvasHeight = canvas.offsetHeight
 
-  const { Engine, Render, Runner, Bodies, Composite, Mouse, MouseConstraint, Events, Body } = Matter
+  const { Engine, Render, Runner, Bodies, Composite, Mouse, MouseConstraint, Constraint, Events, Body } = Matter
 
   let engine, render, runner, mouse, mouseConstraint
-
-//   let circles = []
 
   function init() {
     engine = Engine.create({
@@ -53,32 +50,6 @@ window.addEventListener('DOMContentLoaded', () => {
     Composite.add(engine.world, mouseConstraint)
   }
 
-//   function createLiquid() {
-//     if (circles.length < MAX_CIRCLES) {
-//         const x = 200;
-//         const y = 200;
-//         const radius = randomNumBetween(6, 7);
-//         const body = Bodies.circle(x, y, radius, {
-//             friction: 0,
-//             density: 1,
-//             frictionAir: 0,
-//             restitution: 0.7,
-//             render: {
-//                 fillStyle: '#34d2eb',
-//                 sprite: {
-//                     filter: 'url(#gooey)' // Apply the SVG filter to the circle
-//                 }
-//             }
-//         });
-        
-//         Body.applyForce(body, body.position, { x: 1, y: 0 });
-//         Composite.add(engine.world, body);
-//         circles.push(body);
-//     } else {
-//         console.log("Maximum liquid circles reached!");
-//     }
-//   }
-
   function createWalls()
   {
         const wallColor = '#34d2eb'
@@ -116,16 +87,30 @@ window.addEventListener('DOMContentLoaded', () => {
     // Mass = Density * Area, so Density = Mass / Area
     const density = weight / area; // density in kg/cm²
 
-    // Create the rectangle with the calculated density
-    const cyberTruck = Bodies.rectangle(300, 850, width, height, {
-    density: density, // Apply calculated density to get 10 kg mass
-    render: { fillStyle: '#00000000' }
-    });
-    
-    const headImg = createImageElement('/assets/CyberTruck.png', 'cyberTruck',10);
+    // Create the stack of Cybertrucks
+    const numCybertrucks = 5; // Number of Cybertrucks to stack
+    const initialX = 500; // Starting x position for the stack
+    let initialY = 850; // Starting y position for the first Cybertruck
 
+    for (let i = 0; i < numCybertrucks; i++) {
+    // Create each Cybertruck and stack them vertically
+    const cyberTruck = Bodies.rectangle(initialX, initialY, width, height, {
+        density: density,
+        render: {
+            sprite: {
+              texture: '/assets/CyberTruck.png', // Path to the image
+              xScale: 0.2,  // Scale the image based on the width of the rectangle
+              yScale: 0.2   // Scale the image based on the height of the rectangle
+            }
+        }
+    });
+
+    // Add the Cybertruck to the Matter.js world
     Composite.add(engine.world, cyberTruck);
-    return cyberTruck;
+
+    // Update the y-position for the next Cybertruck to stack it above the previous one
+    initialY -= height; // Move up by the height of one Cybertruck
+    }
   }
 
 
@@ -142,49 +127,6 @@ window.addEventListener('DOMContentLoaded', () => {
     return img;
   }
   
-
-//   function updateImagePositionAndRotation(person) {
-
-//    // Body parts and images should already be mapped
-//    const images = {
-//     head: document.getElementById('head'),
-//     chest: document.getElementById('chest'),
-//     rightUpperArm: document.getElementById('rightUpperArm'),
-//     rightLowerArm: document.getElementById('rightLowerArm'),
-//     leftUpperArm: document.getElementById('leftUpperArm'),
-//     leftLowerArm: document.getElementById('leftLowerArm'),
-//     leftUpperLeg: document.getElementById('leftUpperLeg'),
-//     leftLowerLeg: document.getElementById('leftLowerLeg'),
-//     rightUpperLeg: document.getElementById('rightUpperLeg'),
-//     rightLowerLeg: document.getElementById('rightLowerLeg'),
-//   };
-
-//   // Scale factor (0.3)
-//   const scale = 0.35;
-
-//   // Update position and rotation for each body part
-//   Composite.allBodies(person).forEach(body => {
-//     const img = images[body.label]; // Get the corresponding image
-//     if (img) {
-//       const position = body.position;
-//       const angle = body.angle;
-
-//     // Set the width and height to 0.3 of the original size (scaled)
-//     img.style.width = `${img.naturalWidth * scale}px`;
-//     img.style.height = `${img.naturalHeight * scale}px`;
-
-
-//     // Update position
-//     img.style.left = `${position.x - img.width / 2}px`;
-//     img.style.top = `${position.y - img.height / 2}px`;
-
-//     // Update rotation (convert radians to degrees)
-//     const rotationInDegrees = angle * (180 / Math.PI);
-//     img.style.transform = `rotate(${rotationInDegrees}deg)`;
-//     }
-//   });
-
-//   }
 
   function updateImageCar(car) {
 
@@ -213,387 +155,32 @@ window.addEventListener('DOMContentLoaded', () => {
  
    }
 
-//   function ragdoll(x, y, scale, options) {
-//     scale = typeof scale === 'undefined' ? 1 : scale;
+   function createWreckingBall(){
+    var ball = Bodies.circle(100, 400, 50, { density: 2000, frictionAir: 0.005});
     
-//     var Body = Matter.Body,
-//         Bodies = Matter.Bodies,
-//         Constraint = Matter.Constraint,
-//         Composite = Matter.Composite,
-//         Common = Matter.Common;
-    
-//     var headOptions = Common.extend({
-//         label: 'head',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: [15 * scale, 15 * scale, 15 * scale, 15 * scale]
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var chestOptions = Common.extend({
-//         label: 'chest',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: [20 * scale, 20 * scale, 26 * scale, 26 * scale]
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var leftArmOptions = Common.extend({
-//         label: 'leftUpperArm',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: 10 * scale
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var leftLowerArmOptions = Common.extend({}, leftArmOptions, {
-//         label: 'leftLowerArm',
-//         render: { fillStyle: "#00000000" }
-//     });
-    
-//     var rightArmOptions = Common.extend({
-//         label: 'rightUpperArm',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: 10 * scale
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var rightLowerArmOptions = Common.extend({}, rightArmOptions, {
-//         label: 'rightLowerArm',
-//     });
-    
-//     var leftLegOptions = Common.extend({
-//         label: 'leftUpperLeg',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: 10 * scale
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var leftLowerLegOptions = Common.extend({}, leftLegOptions, {
-//         label: 'leftLowerLeg',
-//         render: { fillStyle: "#00000000" }
-//     });
-    
-//     var rightLegOptions = Common.extend({
-//         label: 'rightUpperLeg',
-//         collisionFilter: {
-//             group: Body.nextGroup(true)
-//         },
-//         chamfer: {
-//             radius: 10 * scale
-//         },
-//         render: { fillStyle: "#00000000" }
-//     }, options);
-    
-//     var rightLowerLegOptions = Common.extend({}, rightLegOptions, {
-//         label: 'rightLowerLeg',
-//         render: { fillStyle: "#00000000" }
-//     });
-    
-//     var head = Bodies.rectangle(x, y - 60 * scale, 34 * scale, 40 * scale, headOptions);
-//     var chest = Bodies.rectangle(x, y, 55 * scale, 80 * scale, chestOptions);
-//     var rightUpperArm = Bodies.rectangle(x + 39 * scale, y - 15 * scale, 20 * scale, 40 * scale, rightArmOptions);
-//     var rightLowerArm = Bodies.rectangle(x + 39 * scale, y + 25 * scale, 20 * scale, 60 * scale, rightLowerArmOptions);
-//     var leftUpperArm = Bodies.rectangle(x - 39 * scale, y - 15 * scale, 20 * scale, 40 * scale, leftArmOptions);
-//     var leftLowerArm = Bodies.rectangle(x - 39 * scale, y + 25 * scale, 20 * scale, 60 * scale, leftLowerArmOptions);
-//     var leftUpperLeg = Bodies.rectangle(x - 20 * scale, y + 57 * scale, 20 * scale, 40 * scale, leftLegOptions);
-//     var leftLowerLeg = Bodies.rectangle(x - 10 * scale, y + 97 * scale, 20 * scale, 60 * scale, leftLowerLegOptions);
-//     var rightUpperLeg = Bodies.rectangle(x + 20 * scale, y + 57 * scale, 20 * scale, 40 * scale, rightLegOptions);
-//     var rightLowerLeg = Bodies.rectangle(x + 20 * scale, y + 97 * scale, 20 * scale, 60 * scale, rightLowerLegOptions);
-    
-//     // Create image elements for each body part (use your own image paths)
-//     const headImg = createImageElement('/assets/head.png', 'head',10);
-//     const chestImg = createImageElement('/assets/body.png', 'chest',9);
-//     const rightUpperArmImg = createImageElement('/assets/rightUpperArm.png', 'rightUpperArm',8);
-//     const rightLowerArmImg = createImageElement('/assets/rightLowerArm.png', 'rightLowerArm',8);
-//     const leftUpperArmImg = createImageElement('/assets/leftUpperArm.png', 'leftUpperArm',8);
-//     const leftLowerArmImg = createImageElement('/assets/leftLowerArm.png', 'leftLowerArm',8);
-//     const leftUpperLegImg = createImageElement('/assets/leftUpperLeg.png', 'leftUpperLeg',8);
-//     const leftLowerLegImg = createImageElement('/assets/leftLowerLeg.png', 'leftLowerLeg',8);
-//     const rightUpperLegImg = createImageElement('/assets/rightUpperLeg.png', 'rightUpperLeg',8);
-//     const rightLowerLegImg = createImageElement('/assets/rightLowerLeg.png', 'rightLowerLeg',8);
-
-//     var chestToRightUpperArm = Constraint.create({
-//         bodyA: chest,
-//         pointA: {
-//             x: 24 * scale,
-//             y: -23 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -8 * scale
-//         },
-//         bodyB: rightUpperArm,
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var chestToLeftUpperArm = Constraint.create({
-//         bodyA: chest,
-//         pointA: {
-//             x: -24 * scale,
-//             y: -23 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -8 * scale
-//         },
-//         bodyB: leftUpperArm,
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var chestToLeftUpperLeg = Constraint.create({
-//         bodyA: chest,
-//         pointA: {
-//             x: -10 * scale,
-//             y: 30 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -10 * scale
-//         },
-//         bodyB: leftUpperLeg,
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var chestToRightUpperLeg = Constraint.create({
-//         bodyA: chest,
-//         pointA: {
-//             x: 10 * scale,
-//             y: 30 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -10 * scale
-//         },
-//         bodyB: rightUpperLeg,
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var upperToLowerRightArm = Constraint.create({
-//         bodyA: rightUpperArm,
-//         bodyB: rightLowerArm,
-//         pointA: {
-//             x: 0,
-//             y: 15 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -25 * scale
-//         },
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var upperToLowerLeftArm = Constraint.create({
-//         bodyA: leftUpperArm,
-//         bodyB: leftLowerArm,
-//         pointA: {
-//             x: 0,
-//             y: 15 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -25 * scale
-//         },
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var upperToLowerLeftLeg = Constraint.create({
-//         bodyA: leftUpperLeg,
-//         bodyB: leftLowerLeg,
-//         pointA: {
-//             x: 0,
-//             y: 20 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -20 * scale
-//         },
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var upperToLowerRightLeg = Constraint.create({
-//         bodyA: rightUpperLeg,
-//         bodyB: rightLowerLeg,
-//         pointA: {
-//             x: 0,
-//             y: 20 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -20 * scale
-//         },
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var headContraint = Constraint.create({
-//         bodyA: head,
-//         pointA: {
-//             x: 0,
-//             y: 25 * scale
-//         },
-//         pointB: {
-//             x: 0,
-//             y: -35 * scale
-//         },
-//         bodyB: chest,
-//         stiffness: 0.6,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var legToLeg = Constraint.create({
-//         bodyA: leftLowerLeg,
-//         bodyB: rightLowerLeg,
-//         stiffness: 0.01,
-//         render: {
-//             visible: false
-//         }
-//     });
-    
-//     var person = Composite.create({
-//         bodies: [
-//             chest, head, leftLowerArm, leftUpperArm, 
-//             rightLowerArm, rightUpperArm, leftLowerLeg, 
-//             rightLowerLeg, leftUpperLeg, rightUpperLeg
-//         ],
-//         constraints: [
-//             upperToLowerLeftArm, upperToLowerRightArm, chestToLeftUpperArm, 
-//             chestToRightUpperArm, headContraint, upperToLowerLeftLeg, 
-//             upperToLowerRightLeg, chestToLeftUpperLeg, chestToRightUpperLeg,
-//             legToLeg
-//         ]
-//     });
-    
-//     return person;
-//     }
-
-//   function createRagdoll() {
-//     const x = 200
-//     const y = 500
-//     const opt = 1.3
-//     var rag = ragdoll(x,y,opt)
-//     Composite.add(engine.world, rag)
-//     return rag
-//   }
-
-//   function Glass() {
-//     const glassImg = document.querySelector('#glass')
-//     this.cx = canvasWidth * 0.5
-//     this.cy = canvasHeight * 0.8
-//     const thickness = 25
-//     const wallColor = '#00000000'
-
-//     const left = Bodies.rectangle(this.cx - 60, this.cy, thickness, 150, {
-//       chamfer: { radius: 10 },
-//       isStatic: true,
-//       angle: Math.PI / 180 * -15,
-//       render: { fillStyle: wallColor }
-//     })
-//     const right = Bodies.rectangle(this.cx + 37, this.cy, thickness, 150, {
-//       chamfer: { radius: 10 },
-//       isStatic: true,
-//       angle: Math.PI / 180 * 15,
-//       render: { fillStyle: wallColor }
-//     })
-//     const bottom = Bodies.rectangle(this.cx - 10, this.cy + 72, 85, thickness * 2, {
-//       chamfer: { radius: 20 },
-//       isStatic: true,
-//       render: { fillStyle: wallColor }
-//     })
-//     glassImg.style.transform = `translate(${this.cx - canvasWidth / 2}px, ${this.cy - canvasHeight / 2}px)`
-
-//     Composite.add(engine.world, [left, right, bottom])
-
-//     this.setPosition = pos => {
-//       this.cx = pos.x
-//       this.cy = pos.y
-//       Body.setPosition(left, { x: this.cx - 60, y: this.cy })
-//       Body.setPosition(right, { x: this.cx + 37, y: this.cy })
-//       Body.setPosition(bottom, { x: this.cx - 10, y: this.cy + 72 })
-//       glassImg.style.transform = `translate(${this.cx - canvasWidth / 2}px, ${this.cy - canvasHeight / 2}px)`
-//     }
-//   }
+    Composite.add(engine.world, ball);
+    Composite.add(engine.world, Constraint.create({
+        pointA: { x: 380, y: 450 },
+        bodyB: ball,
+        stiffness: 0.9,
+        length: 350,
+        render: {
+            strokeStyle: 'black', // Set the constraint line color to black
+            lineWidth: 1 // Optional: you can adjust the line width if needed
+        }
+    }));
+   }
 
   init()
-//   var person = createRagdoll()
   createWalls()
-  var car = createCyberTruck()
-//   resizeFilter()
-//   glass = new Glass()
+  createCyberTruck()
+  createWreckingBall()
 
-//   Events.on(mouseConstraint, "mousemove", e => {
-//     glass.setPosition({
-//       x: e.mouse.position.x - 70,
-//       y: e.mouse.position.y
-//     })
-//   })
-
-//   Events.on(runner, 'tick', e => {
-//     createLiquid()
-//     for (let i = circles.length - 1; i >= 0; i--) {
-//       if (circles[i].position.y - circles[i].circleRadius > canvasHeight) {
-//         Composite.remove(engine.world, circles[i])
-//         circles.splice(i, 1)
-//       }
-//     }
-//   })
 
   Events.on(engine, 'afterUpdate', () => {
-    // updateImagePositionAndRotation(person);
-    updateImageCar(car);
+    //updateImageCar(car);
   });
 
-//   function resizeFilter() {
-//     const feGaussianBlur = document.querySelector('#gooey feGaussianBlur')
-//     const feColorMatrix = document.querySelector('#gooey feColorMatrix')
-//     let index
-//     if (canvasWidth < 600) index = 0
-//     else index = 1
-//     feGaussianBlur.setAttribute('stdDeviation', stdDeviation[index])
-//     feColorMatrix.setAttribute('values', `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${colorMatrix[index]}`)
-//   }
 
   window.addEventListener('resize', () => {
     // canvasWidth = innerWidth
@@ -602,7 +189,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // render.canvas.height = canvasHeight
     // resizeFilter()
 
-    // glass.setPosition({ x: canvasWidth * 0.5, y: canvasHeight * 0.8 })
   })
 
 })
